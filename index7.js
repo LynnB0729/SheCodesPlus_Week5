726fb0c9bad47ec1818d93c17d1daceedda53d57
Lynn <simplylynnhandmade@gmail.com>
2023-03-26 17:48:32 -0400

    deleting files

--- index7.js
+++
@@ -1,9 +0,0 @@
-//let apiKey = "cff2aa1e3c91c0248805f01bed83e69c"; 
-
-//let geoLocationURL = "";
-
-function showPosition(position) {
-    console.log(position);
-}
-
-navigator.geolocation.getCurrentPosition(showPosition);
